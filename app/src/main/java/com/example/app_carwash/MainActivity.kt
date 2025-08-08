9875acecde8d7d0c09aea6c8c23cb47215ac6350
PHCamila21 <camilaalessandra470@gmail.com>
2025-08-08 01:37:34 -0500

    avance1

--- app/src/main/java/com/example/app_carwash/MainActivity.kt
+++ app/src/main/java/com/example/app_carwash/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.app_carwash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menuprincipal, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_mapa -> startActivity(Intent(this, MapaActivity::class.java))
+            R.id.action_quienes -> startActivity(Intent(this, QuienesSomosActivity::class.java))
+            R.id.action_mision -> startActivity(Intent(this, MisionActivity::class.java))
+            R.id.action_vision -> startActivity(Intent(this, VisionActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/app_carwash/MisionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app_carwash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MisionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mision)
+    }
+}
