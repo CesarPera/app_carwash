8f5f55f962524a09b0a1b6207c5bc05634c0e2d5
Cesar Carhuas <cesarcarhuas5@gmail.com>
2025-08-22 05:09:25 -0500

    Revert "Reapply "cami""
    This reverts commit 7a34221fb73352b4d1416a83250415d4f89cd5f6.

--- app services/app/src/main/java/com/example/app_carwash/PersonalActivity.kt
+++ app services/app/src/main/java/com/example/app_carwash/PersonalActivity.kt
@@ -1,21 +1,11 @@
 package com.example.app_carwash
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class PersonalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal)
-
-        val btnRegresar = findViewById<Button>(R.id.btnRegresar)
-
-        btnRegresar.setOnClickListener {
-            // Ir a MainActivity
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
-            finish()
     }
-} }
+}
